1204dd970b68e96c8250721ebd30a9f75882057e
Fright19 <32469983+Fright19@users.noreply.github.com>
2024-05-17 21:59:34 -0400

    Add files via upload

--- searchbarbetterwork.js
+++ searchbarbetterwork.js
@@ -5,15 +5,26 @@ document.getElementById("searchInput").addEventListener("input", function () {
     if (searchTerm === "cobb") {
         outputImage.src = "cobb county.png";
         outputImage.style.display = "block";
-
        
         var link = document.createElement("a");
         link.href = "cobb.html"; 
-
-        
         link.appendChild(outputImage);
-
-        
+        document.body.appendChild(link);
+    } else if (searchTerm === "douglas") {
+        outputImage.src = "douglas county.png";
+        outputImage.style.display = "block";
+       
+        var link = document.createElement("a");
+        link.href = "douglas.html"; 
+        link.appendChild(outputImage);
+        document.body.appendChild(link);
+    } else if (searchTerm === "fulton") {
+        outputImage.src = "fulton county.png"; // Corrected image file path
+        outputImage.style.display = "block";
+       
+        var link = document.createElement("a");
+        link.href = "fulton.html"; // Corrected link href
+        link.appendChild(outputImage);
         document.body.appendChild(link);
     } else {
         outputImage.style.display = "none";
